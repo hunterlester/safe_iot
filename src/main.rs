dcc00dd14ca3d94f17601a0c32e8dd7bdbd58d47
hunterlester <guilfordhunter@gmail.com>
2017-12-02 22:45:52 -0800

    removes submodule, updates deps

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,33 @@
-extern crate safe_core;
-/*
-extern crate dotenv;
-extern crate tokio_core;
-extern crate mio;
+// Use: cargo build --target=arm-unknown-linux-gnueabihf --features="use-mock-routing"
+
+extern crate safe_authenticator;
+extern crate maidsafe_utilities;
+
+#[macro_use]
+extern crate log;
+
+use safe_authenticator::{Authenticator};
+use maidsafe_utilities::log as safe_log;
 
 #[macro_use]
 extern crate unwrap;
 
-use dotenv::dotenv;
-use std::env;
-use std::io;
-use mio::channel;
-
-use safe_core::{Client, CoreError};
-
-use tokio_core::reactor::{Handle, PollEvented, Core};
-
-struct Sender<T> {
-    tx: channel::Sender<T>,
-}
-
-struct Receiver<T> {
-    rx: PollEvented<channel::Receiver<T>>,
-}
-
-impl<T> Clone for Sender<T> {
-    fn clone(&self) -> Sender<T> {
-        Sender { tx: self.tx.clone() }
-    }
-}
-
-
-fn channel2<T>(handle: &Handle) -> io::Result<(Sender<T>, Receiver<T>)>
-    where T: Send + 'static,
-{
-    let (tx, rx) = channel::channel();
-    let rx = try!(PollEvented::new(rx, handle));
-    Ok((Sender { tx: tx }, Receiver { rx: rx }))
-}
-
-
 fn main() {
-    dotenv().expect("Failed to read .env file");
 
-    let mut SEED: String = String::new();
+    unwrap!(safe_log::init(false));
 
-    for (key, value) in env::vars() {
-      if key == "SEED" {
-        SEED = value;
-      }
-    }
+    let username = String::from("someusernamethatmeetsrequirements");
+    let password = String::from("somepasswordthatmeetsrequirements");
+    let invitation = String::from("someinvitation");
 
-    let el = unwrap!(Core::new());
-    let el_h = el.handle();
+    let auth = match Authenticator::create_acc(username.clone(), password.clone(), invitation.clone(), |_| ()) {
+      Ok(authenticator) => authenticator,
+      Err(auth_error) => {
+        panic!("Authentication error: {:?}", auth_error)
+      },
+    };
 
-    let (core_tx, _core_rx) = unwrap!(channel2(&el_h));
-    let (net_tx, _net_rx) = unwrap!(channel2(&el_h));
+    info!("autho info");
 
-    match Client::registered_with_seed::<()>(&SEED,
-                                             el_h.clone(),
-                                             core_tx.clone(),
-                                             net_tx.clone()) {
-      Ok(_) => (),
-      Err(err) => panic!("{:?}", err),
-    }
-
-    println!("Account Created Successfully !!");
-
-}
-*/
-
-fn main() {
-    println!("Hello World!");
+    println!("You are logged in!");
 }
